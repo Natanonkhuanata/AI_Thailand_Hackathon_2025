0d474b14ca80902beb1ff0bcb625f36f3e8ccbb0
NatanonKhuanta (ปราชญ์) <101494144+Natanonkhuanata@users.noreply.github.com>
2025-07-19 01:34:51 +0700

    Update app2.py

--- api2/app2.py
+++ api2/app2.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO)
 @app.route('/')
 def hello():
     logging.info("API2: Received request from API1.")
-    return jsonify({"message": "Hello from API2"})
+    return jsonify("Hello from API2 successfully  connected")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9999)
