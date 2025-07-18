e6b546c04fe7aa6e247f4279caaa49645438c86e
NatanonKhuanta (ปราชญ์) <101494144+Natanonkhuanata@users.noreply.github.com>
2025-07-19 00:20:39 +0700

    Update app1.py

---
+++ api1/app1.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+import requests
+import logging
+
+app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+
+@app.route('/')
+def hello():
+    logging.info("API1: Received request from user.")
+    try:
+        response = requests.get('http://api2:9999/')
+        logging.info(f"API1: Response from API2: {response.text}")
+        return jsonify({
+            "api1": "Hello from API1",
+            "api2_response": response.json()
+        })
+    except Exception as e:
+        logging.error(f"API1: Error calling API2: {e}")
+        return jsonify({"error": "API2 not reachable"}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8888)
